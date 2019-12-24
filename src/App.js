7f28b1c6e285fece877167d16bd983063aa9f4b1
Duncan Laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-12-24 09:53:57 +0000

    intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// components 
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
