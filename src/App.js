da2f8e9c1a7ee96da55a6083b43b36a9bff76c70
Duncan Laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-12-27 08:58:05 +0000

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 // components 
 import HomePage from './components/pages/HomePage/HomePage';
 
+const HatsPage = () => (
+  <div>
+  <h1>HATS PAGE</h1>
+</div>
+)
+ 
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
